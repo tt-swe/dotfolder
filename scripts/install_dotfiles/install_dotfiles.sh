09b653b22568858f840b56735e013b6f7cc4b238
tt-swe <38937418+tt-swe@users.noreply.github.com>
2021-05-19 09:45:45 +0900

    Rename: rename variables

--- scripts/install_dotfiles/install_dotfiles.sh
+++ scripts/install_dotfiles/install_dotfiles.sh
@@ -3,8 +3,8 @@
 echo "Installing dotfiles..."
 
 # variables
-targetdir=~
-newdir=~/dotfolder/dotfiles
+srcdir=~/dotfolder/dotfiles
+outdir=~
 olddir=~/dotfiles_old
 files="bashrc bash_aliases bash_prompt vimrc"
 
@@ -12,16 +12,16 @@ files="bashrc bash_aliases bash_prompt vimrc"
 mkdir -p $olddir
 
 # cd to ~/dotfolder/dotfiles
-cd $newdir
+cd $srcdir
 
 # archive old dotfiles to ~/dotfiles_old and
 # create symbolic links for new dotfiles
 for file in $files; do
-  if [ -e $targetdir/.$file ]; then
-    echo "Archiving '$targetdir/.$file' to '$olddir'"
-    mv $targetdir/.$file $olddir
+  if [ -e $outdir/.$file ]; then
+    echo "Archiving '$outdir/.$file' to '$olddir'"
+    mv $outdir/.$file $olddir
   fi
 
   echo "Creating symbolic link for '.$file'"
-  ln -s $newdir/$file $targetdir/.$file
+  ln -s $srcdir/$file $outdir/.$file
 done
