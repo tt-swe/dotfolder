bccef9a6431d0d8186f375dee5d1e2e2e8e801a7
tt-swe <38937418+tt-swe@users.noreply.github.com>
2021-05-19 09:48:55 +0900

    Rename: rename variables

--- scripts/install_nvim/install_nvim.sh
+++ scripts/install_nvim/install_nvim.sh
@@ -3,28 +3,28 @@
 echo "Installing nvim config files..."
 
 # variables
-nvim_targetdir=~/.config/nvim
-nvim_newdir=~/dotfolder/dotfiles/config/nvim
+nvim_srcdir=~/dotfolder/dotfiles/config/nvim
+nvim_outdir=~/.config/nvim
 nvim_olddir=~/dotfiles_old/config/nvim
 nvim_files="config init.vim coc-settings.json"
 
 # mkdir ~/.config/nvim
-mkdir -p $nvim_targetdir
+mkdir -p $nvim_outdir
 
 # mkdir ~/dotfiles_old/config/nvim
 mkdir -p $nvim_olddir
 
 # cd to ~/dotfolder/dotfiles/config/nvim
-cd $nvim_newdir
+cd $nvim_srcdir
 
 # archive old config files to ~/dotfiles_old/config/nvim and
 # create symbolic links for new config files
 for nvim_file in $nvim_files; do
-  if [ -e $nvim_targetdir/$nvim_file ]; then
-    echo "Archiving '$nvim_targetdir/$nvim_file' to '$nvim_olddir'"
-    mv $nvim_targetdir/$nvim_file $nvim_olddir
+  if [ -e $nvim_outdir/$nvim_file ]; then
+    echo "Archiving '$nvim_outdir/$nvim_file' to '$nvim_olddir'"
+    mv $nvim_outdir/$nvim_file $nvim_olddir
   fi
 
   echo "Creating symbolic link for '.config/nvim/$nvim_file'"
-  ln -s $nvim_newdir/$nvim_file $nvim_targetdir/$nvim_file
+  ln -s $nvim_srcdir/$nvim_file $nvim_outdir/$nvim_file
 done
