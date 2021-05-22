34adbfdcf3611b092812b9a2ef3d113421bd2636
tt-swe <38937418+tt-swe@users.noreply.github.com>
2021-05-23 01:27:28 +0900

    Improve: add packages to be installed

--- scripts/install_packages/install_tools.sh
+++ scripts/install_packages/install_tools.sh
@@ -1,3 +1,13 @@
 packages="curl git gzip unzip tar keychain fzf ripgrep fd-find tree"
 
 apt-get install $packages
+
+# install bat
+# if ripgrep is installed via apt
+# then bat currently cannot be installed via apt
+curl -fLo /tmp/bat_0.18.1_amd64.deb \
+  https://github.com/sharkdp/bat/releases/download/v0.18.1/bat_0.18.1_amd64.deb
+
+dpkg -i /tmp/bat_0.18.1_amd64.deb
+
+rm /tmp/bat_0.18.1_amd64.deb
